82e1da2c1957acf098d5d1ed4c19dfbc41a258a7
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-24 16:04:24 -0300

    feat: records loan routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	bookController "github.com/joaomarcosg/Book-Control-System/internal/books/controllers"
+	loanController "github.com/joaomarcosg/Book-Control-System/internal/loans/controllers"
 	userController "github.com/joaomarcosg/Book-Control-System/internal/users/controllers"
 )
 
@@ -13,9 +14,11 @@ func main() {
 
 	userController := userController.NewUserController()
 	bookController := bookController.NewBooksController()
+	loanController := loanController.NewLoanController()
 
 	userController.RegisterRoutes(router)
 	bookController.RegisterRoutes(router)
+	loanController.RegisterRoutes(router)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
