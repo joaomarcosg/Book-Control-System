3b3344d523d57daaf525bd27f712fcbccfaab40a
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-24 15:41:50 -0300

    chore: adds initial user routes

--- internal/users/controllers/user_controller.go
+++ internal/users/controllers/user_controller.go
@@ -11,9 +11,32 @@ func NewUserController() *UserController {
 func (c *UserController) RegisterRoutes(r *gin.Engine) {
 	users := r.Group("/users")
 
-	users.POST("", c.CreateUser)
+	{
+		users.POST("", c.CreateUser)
+		users.GET("/:id", c.GetUser)
+		users.GET("", c.GetAllUsers)
+		users.PUT("/:id", c.UpdateUser)
+		users.DELETE("/:id", c.DeleteUser)
+	}
+
 }
 
 func (c *UserController) CreateUser(ctx *gin.Context) {
+
+}
+
+func (c *UserController) GetUser(ctx *gin.Context) {
+
+}
+
+func (c *UserController) GetAllUsers(ctx *gin.Context) {
+
+}
+
+func (c *UserController) UpdateUser(ctx *gin.Context) {
+
+}
+
+func (c *UserController) DeleteUser(ctx *gin.Context) {
 
 }
