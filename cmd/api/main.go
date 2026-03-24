998b4dd851caccdc8df6f866c7a163ce281028b2
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-24 15:58:59 -0300

    feat: records book routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,18 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joaomarcosg/Book-Control-System/internal/users/controllers"
+	bookController "github.com/joaomarcosg/Book-Control-System/internal/books/controllers"
+	userController "github.com/joaomarcosg/Book-Control-System/internal/users/controllers"
 )
 
 func main() {
 	router := gin.Default()
 
-	userController := controllers.NewUserController()
+	userController := userController.NewUserController()
+	bookController := bookController.NewBooksController()
+
 	userController.RegisterRoutes(router)
+	bookController.RegisterRoutes(router)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
