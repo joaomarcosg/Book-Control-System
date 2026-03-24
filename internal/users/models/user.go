94a4150cbf228913ece25b55fbd9232ff3787ab2
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-24 16:11:43 -0300

    feat: adds User entity

---
+++ internal/users/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        int64     `json:"ID"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"Updated_at"`
+}
