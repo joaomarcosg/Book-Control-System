a0fe716ea466856cc3445a27f94a03a28dabfc8b
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-26 16:09:11 -0300

    feat: adds postgresql connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 	bookController "github.com/joaomarcosg/Book-Control-System/internal/books/controllers"
 	loanController "github.com/joaomarcosg/Book-Control-System/internal/loans/controllers"
 	userController "github.com/joaomarcosg/Book-Control-System/internal/users/controllers"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("BOOK_CONTROL_SYSTEM_DATABASE_USER"),
+		os.Getenv("BOOK_CONTROL_SYSTEM_DATABASE_PASSWORD"),
+		os.Getenv("BOOL_CONTROL_SYSTEM_DATABASE_HOST"),
+		os.Getenv("BOOK_CONTROL_SYSTEM_DATABASE_PORT"),
+		os.Getenv("BOOK_CONTROL_SYSTEM_DATABASE_NAME"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
 	router := gin.Default()
 
 	userController := userController.NewUserController()
