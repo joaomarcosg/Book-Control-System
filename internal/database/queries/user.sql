c108ff4932318e3f1b5d39297ee9c4181cc9b54b
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-03-26 16:45:06 -0300

    feat: adds user sqlc queries

---
+++ internal/database/queries/user.sql
@@ -0,0 +1,29 @@
+-- name: CreateUser :one
+INSERT INTO users (
+    name,
+    email
+)
+VALUES ($1, $2)
+RETURNING id, name, email, created_at;
+
+-- name: GetUser :one
+SELECT id, name, email, created_at
+FROM users
+WHERE id = $1;
+
+-- name: GetAllUsers :many
+SELECT id, name, email, created_at
+FROM users
+ORDER BY id;
+
+-- name: UpdateUser :one
+UPDATE users
+SET
+    name = COALESCE($2, name),
+    email = COALESCE($3, email),
+WHERE id = $1;
+RETURNING id, name, email, created_at;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
