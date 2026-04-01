de2b6ea5ff5ee35e9f5268a93dd9d23329628b5e
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-01 17:14:45 -0300

    refactor: refactor mapper function

--- internal/users/repositories/user_mapper.go
+++ internal/users/repositories/user_mapper.go
@@ -5,13 +5,12 @@ import (
 	"github.com/joaomarcosg/Book-Control-System/internal/users/models"
 )
 
-func toDomain(u sqlc.User) *models.User {
+func toDomainFromCreate(u sqlc.CreateUserRow) *models.User {
 	return &models.User{
 		ID:        int64(u.ID),
 		Name:      u.Name,
 		Email:     u.Email,
 		CreatedAt: u.CreatedAt.Time,
-		UpdatedAt: u.UpdatedAt.Time,
 	}
 }
 
