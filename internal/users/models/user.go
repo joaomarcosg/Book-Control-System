dcd411cc8dba1f9c15fa82fdbb43fd1398251962
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-02 16:18:43 -0300

    fix: fixes json tag

--- internal/users/models/user.go
+++ internal/users/models/user.go
@@ -7,5 +7,5 @@ type User struct {
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"Updated_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
