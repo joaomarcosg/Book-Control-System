5bd374f25e2995043262f730099b0923f7e4a35a
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-03 18:51:22 -0300

    refactor: refactor mapper functions

--- internal/users/repositories/user_mapper.go
+++ internal/users/repositories/user_mapper.go
@@ -1,16 +1,24 @@
 package repositories
 
 import (
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/joaomarcosg/Book-Control-System/internal/database/sqlc"
 	"github.com/joaomarcosg/Book-Control-System/internal/users/models"
 )
 
-func toDomainFromCreate(u sqlc.CreateUserRow) *models.User {
+func mapToDomain(
+	id int32,
+	name,
+	email string,
+	createdAt,
+	updatedAt pgtype.Timestamptz,
+) *models.User {
 	return &models.User{
-		ID:        int64(u.ID),
-		Name:      u.Name,
-		Email:     u.Email,
-		CreatedAt: u.CreatedAt.Time,
+		ID:        int64(id),
+		Name:      name,
+		Email:     email,
+		CreatedAt: createdAt.Time,
+		UpdatedAt: updatedAt.Time,
 	}
 }
 
@@ -19,4 +27,24 @@ func toCreateParams(u *models.User) sqlc.CreateUserParams {
 		Name:  u.Name,
 		Email: u.Email,
 	}
+}
+
+func toDomain(u sqlc.User) *models.User {
+	return mapToDomain(
+		u.ID,
+		u.Name,
+		u.Email,
+		u.CreatedAt,
+		u.UpdatedAt,
+	)
+}
+
+func toDomainList(users []sqlc.User) []*models.User {
+	var result []*models.User
+
+	for _, u := range users {
+		result = append(result, toDomain(u))
+	}
+
+	return result
 }
