b385f824bdaff513ffe0a26140a606a5ce6ecbaf
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-03 19:05:19 -0300

    feat: implements CreateUser, GetUser and GetAllUsers interface methods

--- internal/users/repositories/user_repository_pg.go
+++ internal/users/repositories/user_repository_pg.go
@@ -2,7 +2,10 @@ package repositories
 
 import (
 	"context"
+	"errors"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	db "github.com/joaomarcosg/Book-Control-System/internal/database/sqlc"
 	"github.com/joaomarcosg/Book-Control-System/internal/users/models"
 )
@@ -11,33 +14,57 @@ type userRepository struct {
 	queries *db.Queries
 }
 
-// CreateUser implements [UserRepository].
-func (u *userRepository) CreateUser(ctx context.Context, user models.User) (models.User, error) {
-	panic("unimplemented")
-}
-
-// DeleteUser implements [UserRepository].
-func (u *userRepository) DeleteUser(ctx context.Context, id int64) error {
-	panic("unimplemented")
-}
-
-// GetAllUsers implements [UserRepository].
-func (u *userRepository) GetAllUsers(ctx context.Context) ([]models.User, error) {
-	panic("unimplemented")
-}
-
-// GetUser implements [UserRepository].
-func (u *userRepository) GetUser(ctx context.Context, id int64) (models.User, error) {
-	panic("unimplemented")
-}
-
-// UpdateUser implements [UserRepository].
-func (u *userRepository) UpdateUser(ctx context.Context, id int64, user models.User) error {
-	panic("unimplemented")
-}
-
 func NewUserRepository(q *db.Queries) UserRepository {
 	return &userRepository{
 		queries: q,
 	}
+}
+
+func (u *userRepository) CreateUser(ctx context.Context, user *models.User) (int64, error) {
+
+	params := toCreateParams(user)
+	id, err := u.queries.CreateUser(ctx, params)
+
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return 0, ErrDuplicateUserNameOrEmail
+		}
+		return 0, err
+	}
+
+	return int64(id), nil
+}
+
+func (u *userRepository) GetUser(ctx context.Context, id int64) (*models.User, error) {
+
+	user, err := u.queries.GetUser(ctx, int32(id))
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrUserNotFound
+		}
+		return nil, err
+	}
+
+	return toDomain(user), nil
+}
+
+func (u *userRepository) GetAllUsers(ctx context.Context) ([]*models.User, error) {
+
+	users, err := u.queries.GetAllUsers(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return toDomainList(users), nil
+
+}
+
+func (u *userRepository) UpdateUser(ctx context.Context, id int64, user *models.User) error {
+	panic("unimplemented")
+}
+
+func (u *userRepository) DeleteUser(ctx context.Context, id int64) error {
+	panic("unimplemented")
 }
