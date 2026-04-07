b4a2c7c534e859c1d302fa1de7f15479ce8e8e38
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-07 16:59:14 -0300

    feat: adds CreateUser service

--- internal/users/services/user_service.go
+++ internal/users/services/user_service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"context"
+
+	"github.com/joaomarcosg/Book-Control-System/internal/users/models"
 	"github.com/joaomarcosg/Book-Control-System/internal/users/repositories"
 )
 
@@ -12,4 +15,32 @@ func NewUserService(userRepo repositories.UserRepository) repositories.UserRepos
 	return &UserService{
 		userRepo: userRepo,
 	}
+}
+
+func (u *UserService) CreateUser(ctx context.Context, user *models.User) (int64, error) {
+
+	id, err := u.userRepo.CreateUser(ctx, user)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+
+}
+
+func (u *UserService) DeleteUser(ctx context.Context, id int64) error {
+	panic("unimplemented")
+}
+
+func (u *UserService) GetAllUsers(ctx context.Context) ([]*models.User, error) {
+	panic("unimplemented")
+}
+
+func (u *UserService) GetUser(ctx context.Context, id int64) (*models.User, error) {
+	panic("unimplemented")
+}
+
+func (u *UserService) UpdateUser(ctx context.Context, id int64, user *models.User) error {
+	panic("unimplemented")
 }
