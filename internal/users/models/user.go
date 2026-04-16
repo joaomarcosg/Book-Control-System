c2caee59a45f2e21d81bc887bd2f69136b84b583
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-16 16:04:29 -0300

    feat: adds input validation

--- internal/users/models/user.go
+++ internal/users/models/user.go
@@ -4,8 +4,8 @@ import "time"
 
 type User struct {
 	ID        int64     `json:"ID"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
+	Name      string    `json:"name" binding:"required,min=5,max=200"`
+	Email     string    `json:"email" binding:"required,email"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
